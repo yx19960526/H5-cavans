58d5ea5b2dd80db8ffdeaa0eb89c9c6969013f81
yx <943309795@qq.com>
2018-03-18 22:32:31 +0800

    提交

---
+++ js/clock.js
@@ -0,0 +1,148 @@
+var dom = document.getElementById('clock');
+var ctx = dom.getContext('2d');
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+var r   = width / 2;
+/**
+ * 获取元素上下文
+ */
+
+/**
+ * 绘制背景
+ */
+function drawBackground(){
+	ctx.save();
+	ctx.translate(r,r);
+	ctx.beginPath();
+	/**
+	 * 线条大小
+	 */
+	ctx.lineWidth = 10;
+	/**
+	 * 画圆
+	 */
+	ctx.arc(0,0,r -5,0,2*Math.PI,false);
+	ctx.stroke();
+	/**
+	 * 根据半径填充数字
+	 */
+	var hourNumber = [3,4,5,6,7,8,9,10,11,12,1,2];
+	ctx.font = '18px Arial';
+	ctx.textAlign = 'center';
+	ctx.textBaseline = 'middle';
+	hourNumber.forEach(function(number,i){
+		var rad = 2 * Math.PI / 12 * i;
+		var x = Math.cos(rad) * (r - 30);
+		var y = Math.sin(rad) * (r - 30);
+		ctx.fillText(number,x,y,);	
+	});	
+	/**
+	 * 画圆点，并且可以除以5没有余数的设置为黑色
+	 */
+	for (var i=0; i< 60;i++){
+		var rad = 2 * Math.PI /60 * i;
+		var x = Math.cos(rad) * (r - 18);
+		var y = Math.sin(rad) * (r - 18);
+		ctx.beginPath();
+		if(i % 5 ===0){
+			ctx.fillStyle = 'crimson';
+			ctx.arc(x,y,2,0,2*Math.PI,false);
+		}else{
+			ctx.fillStyle = '#ccc';
+			ctx.arc(x,y,2,0,2*Math.PI,false);
+		}
+		ctx.fill();
+	}
+}
+/**
+ * 画时针
+ * @param {Object} hour 小时数
+ * @param {Object} minute 分钟数
+ * 
+ * 根据小时数和分钟数 设计时针的弧度
+ */
+function dramHour(hour,minute){
+	ctx.save();
+	ctx.beginPath();
+	var rad = 2 * Math.PI / 12 * hour;
+	var mrad = 2 *Math.PI / 12 /60 * minute
+	ctx.rotate(rad + mrad);
+	ctx.lineWidth =6;
+	ctx.lineCap = 'round';
+	ctx.moveTo(0,10);
+	ctx.lineTo(0,-r / 2);
+	ctx.stroke();
+	ctx.restore();
+}
+
+/**
+ * 画分针
+ * @param {Object} minute
+ * 根据分针设计 分针的弧度
+ */
+function dramMinute(minute){
+	ctx.save();
+	ctx.beginPath();
+	var rad = 2 * Math.PI / 60 * minute;
+	ctx.rotate(rad);
+	ctx.lineWidth =3;
+	ctx.lineCap = 'round';
+	ctx.moveTo(0,10);
+	ctx.lineTo(0,-r + 35);
+	ctx.stroke();
+	ctx.restore();
+}
+/**
+ * 秒针
+ * @param {Object} second
+ * 同理
+ */
+function dramSecond(second){
+	ctx.save();
+	ctx.beginPath();
+	ctx.fillStyle = 'red';
+	var rad = 2 * Math.PI / 60 * second;
+	ctx.rotate(rad);
+	ctx.lineWidth = 1;
+	ctx.lineCap = 'round';
+	ctx.moveTo(0,10);
+	ctx.lineTo(0,-r + 25);
+	ctx.stroke();
+	ctx.restore();
+}
+
+/**
+ * 画时针分针秒针的交叉点
+ */
+function drawDot(){
+	
+	ctx.beginPath();
+	ctx.fillStyle = '#fff';
+	ctx.arc(0,0,2,0,2*Math.PI,false);
+	ctx.fill();
+}
+
+/**
+ * 画整个时钟
+ */
+function draw(){
+	ctx.clearRect(0,0,width,height);
+	var now = new Date();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+		drawBackground();
+	dramHour(hour,minute);
+	dramMinute(minute);
+	dramSecond(second);
+	drawDot();
+	ctx.restore();
+}
+/*
+ * 初始时钟
+ */
+draw();
+/*
+ * 让时钟动起来
+ */
+setInterval(draw,1000);
